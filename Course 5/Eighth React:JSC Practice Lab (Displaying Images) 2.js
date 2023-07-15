415b3387636271e35227ef097748ddf1673596a9
Yasmin Shafik <jshafik6@gmail.com>
2023-07-15 11:00:47 -0400

    Add files via upload

---
+++ Course 5/Eighth React:JSC Practice Lab (Displaying Images) 2.js
@@ -0,0 +1,12 @@
+import logo from"./assets/logo.png"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Little Lemon Logo</h1>
+      <img src={logo} alt="Logo" />
+    </div>
+  );
+};
+
+export default App;
